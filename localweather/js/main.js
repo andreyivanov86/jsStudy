f814239bc15315ab0eec5c719a378bbb122286b6
AndreyI <Andreyivanov86@gmail.com>
2017-04-03 13:39:58 -0400

    display location

--- localweather/js/main.js
+++ localweather/js/main.js
@@ -1,10 +1,29 @@
 window.onload = function () { 
-    if ("geolocation" in navigator){ 
-        
-        navigator.geolocation.getCurrentPosition(function(position){
-                $("#location").html(position.coords.latitude+"\n<br>"+ position.coords.longitude);
-            });
-    }else{
-        console.log("Browser doesn't support geolocation!");
+    if (navigator.geolocation){         
+        navigator.geolocation.getCurrentPosition(getGeolocationSuccess, getGeolcationFailed);
     }
+    
+
+
+    function getGeolocationSuccess(position) {
+    	var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + 
+        latitude + '%2C' + longitude + '&language=en';
+
+        $.getJSON(geocoding).done(function(location){
+        	$("#location").html(location.results[0].address_components[2].long_name + ", " + 
+        		location.results[0].address_components[4].long_name)
+        })
+
+        // $("#location").html(latitude +"\n<br>"+ longitude);
+    };
+
+    function getGeolcationFailed () {
+    	alert("Geocoder failed");
+    };
+
+
+
 };
