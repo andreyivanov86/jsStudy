9064751aaa8ce473f22321717497bc13a86e2309
AndreyI <Andreyivanov86@gmail.com>
2017-04-03 17:02:05 -0400

    mede the weather work with simpleweather API

--- localweather/js/main.js
+++ localweather/js/main.js
@@ -1,29 +1,51 @@
 window.onload = function () { 
     if (navigator.geolocation){         
-        navigator.geolocation.getCurrentPosition(getGeolocationSuccess, getGeolcationFailed);
+        
+        //navigator.geolocation.getCurrentPosition(getGeolocationSuccess, getGeolcationFailed);
+        
+        navigator.geolocation.getCurrentPosition(function(position){
+        	 loadWeather(position.coords.latitude + ',' + position.coords.longitude);
+        })
     }
     
 
 
-    function getGeolocationSuccess(position) {
-    	var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
+    // function getGeolocationSuccess(position) {
+    // 	var latitude = position.coords.latitude;
+    //     var longitude = position.coords.longitude;
 
-        var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + 
-        latitude + '%2C' + longitude + '&language=en';
+    //     var geocoding = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + 
+    //     latitude + '%2C' + longitude + '&language=en';
 
-        $.getJSON(geocoding).done(function(location){
-        	$("#location").html(location.results[0].address_components[2].long_name + ", " + 
-        		location.results[0].address_components[4].long_name)
-        })
-
-        // $("#location").html(latitude +"\n<br>"+ longitude);
-    };
+    //     $.getJSON(geocoding).done(function(location){
+    //     	$("#location").html(location.results[0].address_components[2].long_name + ", " + 
+    //     		location.results[0].address_components[4].long_name)
+    //     })
+    //  };
 
     function getGeolcationFailed () {
     	alert("Geocoder failed");
     };
 
+    function loadWeather(location, woeid) {
+    	$.simpleWeather({
+    		location: location,
+    		woeid: woeid,
+    		unit: "f",
+    		success: function(weather) {
+
+    			htmlLocation = weather.city + ", " + weather.region;
+    			htmlWeather = '<i class="wi wi-yahoo' + weather.code + '"></i>' + weather.temp + '&deg;' + 
+    			weather.units.temp;
+    			htmlWind = weather.currently;
+    			
+
+    			$("#location").html(htmlLocation);
+    			$("#weather").html(htmlWeather);
+    			$("#wind").html(htmlWind);
+    		}
+    	})
+    }
 
 
 };
