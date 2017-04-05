6bd6924f30c97757bbab257c14690ccc490a829b
AndreyI <Andreyivanov86@gmail.com>
2017-04-05 18:25:39 -0400

    weather progress

--- localweather/js/main.js
+++ localweather/js/main.js
@@ -1,40 +1,40 @@
-window.onload = function () { 
+$(document).ready(function () { 
     if (navigator.geolocation){         
                 //navigator.geolocation.getCurrentPosition(getGeolocationSuccess, getGeolcationFailed);
         navigator.geolocation.getCurrentPosition(function(position){
-        	 loadWeather(position.coords.latitude + ',' + position.coords.longitude);
+             loadWeather(position.coords.latitude + ',' + position.coords.longitude);
         })
     }
 
     // function getGeolcationFailed () {
-    // 	alert("Geocoder failed");
+    //  alert("Geocoder failed");
     // };
 
     function loadWeather(location, woeid) {
-    	$.simpleWeather({
-    		location: location,
-    		woeid: woeid,
-    		unit: "f",
-    		
-    		success: function(weather) {
+        $.simpleWeather({
+            location: location,
+            woeid: woeid,
+            unit: "f",
+            
+            success: function(weather) {
 
-    			
-    			htmlLocation = weather.city + ", " + weather.region;
-    			htmlWeather = '<h2 class = "icon-5"></h2>' + weather.code + " " + weather.temp + '&deg' + 
-    			weather.units.temp;
-    			htmlWind = weather.currently;
-    			htmlWindchill = "Wind " + weather.wind.chill + "&deg ";
+                
+                htmlLocation = weather.city + ", " + weather.region;
+                htmlWeather = '<i class = "icon-' + weather.code + '"></i>' + weather.temp + '&deg' + 
+                weather.units.temp;
+                htmlWind = weather.currently;
+                htmlWindchill = "Wind " + weather.wind.chill + "&deg ";
                 htmlWindspeed =  weather.wind.direction + " " + Math.round(weather.wind.speed) + " " + weather.units.speed;
 
-    			
-    			$("#location").html(htmlLocation);
-    			$("#weather").html(htmlWeather);
-    			$("#conditions").html(htmlWind);
+                
+                $("#location").html(htmlLocation);
+                $("#weather").html(htmlWeather);
+                $("#conditions").html(htmlWind);
                 $("#wind-chill").html(htmlWindchill);
-    			$("#wind-speed").html(htmlWindspeed)
-    		}
-    	})
+                $("#wind-speed").html(htmlWindspeed)
+            }
+        })
     }
-};
+})
 
     
