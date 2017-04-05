56b4ccbbc4b09d2252f015e7bf2401177fa113de
AndreyI <Andreyivanov86@gmail.com>
2017-04-05 10:09:27 -0400

    localweather adjusted to mobile

--- localweather/js/main.js
+++ localweather/js/main.js
@@ -14,23 +14,24 @@ window.onload = function () {
     	$.simpleWeather({
     		location: location,
     		woeid: woeid,
-    		unit: "c",
+    		unit: "f",
     		
     		success: function(weather) {
 
     			//htmlWeathericon = ;
     			htmlLocation = weather.city + ", " + weather.region;
-    			htmlWeather = '<h2><i class="icon-'+weather.code+'"></i>' + weather.temp + '&deg;' + 
+    			htmlWeather = '<h2><i class="icon-'+weather.code+'"></i>' + " " + weather.temp + '&deg;' + 
     			weather.units.temp + '</h2>';
     			htmlWind = weather.currently;
-    			htmlWindspeed = "Wind " + weather.wind.chill + "&deg " + " " + weather.wind.direction + " " + weather.wind.speed + " " +
-    			weather.units.speed;
+    			htmlWindchill = "Wind " + weather.wind.chill + "&deg ";
+                htmlWindspeed =  weather.wind.direction + " " + Math.round(weather.wind.speed) + " " + weather.units.speed;
 
     			
     			$("#location").html(htmlLocation);
     			//$("#weather-icon").html(htmlWeathericon);
     			$("#weather").html(htmlWeather);
-    			$("#wind").html(htmlWind);
+    			$("#conditions").html(htmlWind);
+                $("#wind-chill").html(htmlWindchill);
     			$("#wind-speed").html(htmlWindspeed)
     		}
     	})
