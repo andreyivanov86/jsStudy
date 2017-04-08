446813efd243d88787395cd4c917b2a1bffc052a
AndreyI <Andreyivanov86@gmail.com>
2017-04-08 15:43:28 -0400

    weather forecast progress

--- localweather/js/main.js
+++ localweather/js/main.js
@@ -48,24 +48,38 @@ $(document).ready(function () {
             success: function(weather) {
                 globalWeather = weather;
                 console.log(weather);
+                //Current Weather
                 htmlLocation = weather.city + ", " + weather.region;
                 htmlWeather = '<i class = "icon-' + weather.code + '"></i>' + " " + weather.temp + ' &deg' + 
                 weather.units.temp;
                 htmlWind = weather.currently;
                 htmlWindchill = "Wind " + weather.wind.chill + "&deg ";
                 htmlWindspeed =  weather.wind.direction + " " + Math.round(weather.wind.speed) + " " + weather.units.speed;
-                //htmlForcast = weather.forcast[1];
-                
-                console.log(weather);
-                
-
+                //display current weather
                 $("#location").html(htmlLocation);
                 $("#weather").html(htmlWeather);
                 $("#conditions").html(htmlWind);
                 $("#wind-chill").html(htmlWindchill);
                 $("#wind-speed").html(htmlWindspeed)
 
-                //$("#day1").html(htmlForcast);
+                //Forecast weather
+                htmlForecast1 = weather.forecast[1].day + "<br>" + '<i class = "forecast-icon icon-' + weather.code + '"></i>' + "<br>"
+                + "High " + weather.forecast[1].high + "&deg "+ weather.units.temp + "<br>" + "Low " + weather.forecast[1].low + 
+                "&deg "+ weather.units.temp + "<br>" + weather.forecast[1].text;
+                htmlForecast2 = weather.forecast[2].day + "<br>" + '<i class = "forecast-icon icon-' + weather.code + '"></i>' + "<br>"
+                + "High " + weather.forecast[2].high + "&deg "+ weather.units.temp + "<br>" + "Low " + weather.forecast[2].low + 
+                "&deg "+ weather.units.temp + "<br>" + weather.forecast[2].text;
+                htmlForecast3 = weather.forecast[3].day + "<br>" + '<i class = "forecast-icon icon-' + weather.code + '"></i>' + "<br>"
+                + "High " + weather.forecast[3].high + "&deg "+ weather.units.temp + "<br>" + "Low " + weather.forecast[3].low + 
+                "&deg "+ weather.units.temp + "<br>" + weather.forecast[3].text;
+                htmlForecast4 = weather.forecast[4].day + "<br>" + '<i class = "forecast-icon icon-' + weather.code + '"></i>' + "<br>"
+                + "High " + weather.forecast[4].high + "&deg "+ weather.units.temp + "<br>" + "Low " + weather.forecast[4].low + 
+                "&deg "+ weather.units.temp + "<br>" + weather.forecast[4].text;    
+                //Display Forecast
+                $("#day1").html(htmlForecast1);
+                $("#day2").html(htmlForecast2);
+                $("#day3").html(htmlForecast3);
+                $("#day4").html(htmlForecast4);
 
                 if (weather.temp <= 35){ 
                     $("#body").css("background-color", "#1E90FF"); 
